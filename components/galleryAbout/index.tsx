f3feb9ee823277140c3d4559c261d776ba228a26
JIaIIyX <96249976+JIaIIyX@users.noreply.github.com>
2024-04-17 12:15:12 +0300

    Update index.tsx

--- components/galleryAbout/index.tsx
+++ components/galleryAbout/index.tsx
@@ -75,12 +75,12 @@ Curabitur sodales ex velit, vel fringilla risus ultrices sed. Quisque laoreet su
               onMouseEnter={() => {
                 if ( index + rowIndex * 7 !== hoveredIndex) setHoveredIndex( index + rowIndex * 7);
               }}
-              className={`w-[360px]  h-[420px] absolute transition-all duration-300 ease-in-out `}
+              className={`w-[400px]  h-[420px] absolute transition-all duration-300 ease-in-out `}
             >
               <img
                 src={getImageSrc(item.image)}
                 alt={item.name}
-                className="h-full bg-red-100 w-full"
+                className={`h-full bg-red-100 w-full relative  transition-all duration-500 ease-in-out ${hoveredIndex ===  index + rowIndex * 7 ? "right-0" : "right-[25%]" }`}
               />
             </Link>
           </li>
