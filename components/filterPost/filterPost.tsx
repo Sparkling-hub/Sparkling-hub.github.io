04d7c68e930d8adcd2ba455c0f77dd2d0cec26fe
JIaIIyX <96249976+JIaIIyX@users.noreply.github.com>
2024-05-23 18:51:00 +0300

    Update filterPost.tsx

--- components/filterPost/filterPost.tsx
+++ components/filterPost/filterPost.tsx
@@ -22,7 +22,7 @@ const Filter: React.FC = () => {
 
 	return (
 		<div className="filter-container bg-gray-100 my-6 sm:mx-5 w-4/5 sm:w-2/4 lg:w-1/4 z-20">
-			<h3 className="text-center text-3xl mt-10 mb-5  ">Open Position</h3>
+			<h3 className="text-center text-3xl mt-10 mb-5  ">Open Post</h3>
 			<div className="text-sm">
 				<div className={`my-multiple-select mx-4`} >
 					<div className="relative my-multiple-select-container w-full flex-col flex rounded-lg relative bg-color-primary-medium border-bg-color-primary-medium">
