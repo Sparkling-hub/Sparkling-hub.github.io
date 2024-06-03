79eec190096a4e279b18e93dcbac321543c64599
JIaIIyX <96249976+JIaIIyX@users.noreply.github.com>
2024-06-04 02:13:28 +0300

    Update section_one.tsx

--- data/data-sections/data-section-admin/section_one.tsx
+++ data/data-sections/data-section-admin/section_one.tsx
@@ -1,13 +1,10 @@
 
-import Reg from '@/components/registration/registration'
+
 import Auth from '@/components/authorization/authorization'
-const header =
-  <h1 className="text-5xl my-4 mx-1 text-primary-darkTeal font-bold mb-10"></h1>
+
  
 const content = <div className='flex'>
-  <div  className='w-1/2 px-[10%] border-r-2 border-green-800'><Auth/></div>
-<div className='w-1/2 ps-[10%] border-l-2 border-green-800'><Reg/></div>
-
+  <div  className='w-[500px] m-auto  border-green-800'><Auth/></div>
 </div>
 
 
@@ -15,7 +12,6 @@ const content = <div className='flex'>
 
 const element = {
   content: content,
-  header: header,
   id: "about"
 
 }
