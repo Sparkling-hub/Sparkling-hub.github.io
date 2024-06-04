1271a0af82efa5cd0b6342e0c72d5afc68897016
JIaIIyX <96249976+JIaIIyX@users.noreply.github.com>
2024-06-04 20:00:14 +0300

    Update Card.tsx

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -56,17 +56,18 @@ const Card: React.FC = () => {
   return (
     <div ref={cardRef}
       className={` before:border-transparent
-
+      drop-shadow-[0_3px_4px_rgba(0,0,0,0.23)]
       before:border-b-[#B0D311] before:border-r-[100px] 
       z-[5]
       before:transform before:rotate-180
       before:absolute
-
+      transition-all duration-300 ease-in-out 
       before:rounded-b-3xl
       before:border-l-[100px]
       before:w-full
       before:z-[-1]
       before:-translate-x
+     
       before:overflow-visible
       before:transition-all before:duration-300 before:ease-in-out 
       absolute w-full text-black w-screen max-w-[200px]  
@@ -76,9 +77,7 @@ const Card: React.FC = () => {
       style={{
         top: `calc(${Math.round(activeOfficePointCoords[0] - 199)}px)`,
         left: `calc(${Math.round(activeOfficePointCoords[1] - 85)}px)`
-    }}><div className={`overflow-hidden flex items-center 
-   
-    drop-shadow-[0_3px_4px_rgba(0,0,0,0.23)] justify-center relative lg:absolute w-full bg-[#B0D311] duration-300 rounded-3xl ease-out  transition-height  ${isVisible ? 'opacity-100 h-[100%] top-[0%] ' : '100 h-[100%] top-[0%]'}`} >
+    }}><div className={`overflow-hidden flex items-center justify-center relative lg:absolute w-full bg-[#B0D311] duration-300 rounded-3xl ease-out  transition-height  ${isVisible ? 'opacity-100 h-[100%] top-[0%] ' : '100 h-[100%] top-[0%]'}`} >
         <div className={`w-full left-0 px-6`}>
 
 
