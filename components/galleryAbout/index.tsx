eb67b660c8f65f2003312041f93f52f353c3f883
JIaIIyX <96249976+JIaIIyX@users.noreply.github.com>
2024-06-09 05:46:56 +0300

    Update index.tsx

--- components/galleryAbout/index.tsx
+++ components/galleryAbout/index.tsx
@@ -50,7 +50,7 @@ Curabitur sodales ex velit, vel fringilla risus ultrices sed. Quisque laoreet su
 
       <div className="">
       {[...Array(rows)].map((_, rowIndex) => (
-        <div className="flex flex-row" key={'rows'}>
+        <div className="flex flex-row" key={'rows'+ rowIndex}>
       {dataImages
             .slice(rowIndex * 7, rowIndex * 7 + 7)
             .map((item, index) => (
