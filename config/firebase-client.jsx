108c89273192714cf384d94e0df3235608ab28bf
Lian Arthofer <77974438+LianArthofer@users.noreply.github.com>
2024-06-28 11:51:53 +0100

    fix

--- config/firebase-client.jsx
+++ config/firebase-client.jsx
@@ -1,12 +1,13 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-
 import { getStorage } from 'firebase/storage';
+
+import NEXT_PUBLIC_FIREBASE_API_KEY from '@env';
 
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
