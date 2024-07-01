009fdefb85affa33831dbc7f9ac3548a440d1c7b
JIaIIyX <96249976+JIaIIyX@users.noreply.github.com>
2024-07-01 07:59:08 +0300

    Update tags-input.tsx

--- components/ui/tags-input/tags-input.tsx
+++ components/ui/tags-input/tags-input.tsx
@@ -139,7 +139,7 @@ const MyMultipleSelect: React.FC = () => {
           <div className='w-full '>
             <input
               type="text"
-              className='bg-primary-lightTeal rounded-xl p-2 focus:outline-none w-full'
+              className='p-2 focus:outline-none w-full'
               value={inputValue}
               onChange={handleInputChange}
               onKeyDown={handleKeyDown}
